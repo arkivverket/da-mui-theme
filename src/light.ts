e6e0029b5af1bb0dd5ecdbb030f09d9358c2c954
Sigmund Lahn <siglah@arkivverket.no>
2023-03-09 22:15:13 +0100

    Add extra color definitions and their values

--- src/light.ts
+++ src/light.ts
@@ -1,15 +1,51 @@
 import { createTheme } from "@mui/material/styles"
 
+/**
+ * Extra colors used by Arkivverket
+ */
+declare module "@mui/material/styles" {
+	interface TypeText {
+		"25p": string
+		"50p": string
+		white: string
+	}
+
+	interface SimplePaletteColorOptions {
+		"25p": string
+		"50p": string
+	}
+
+	interface TypeBackground {
+		black: string
+		white: string
+		disabled: string
+		border: string
+		hover: string
+		selected: string
+	}
+}
+/** 
+ * The Arkivverket MUI theme
+ */
 const theme = createTheme({
 	palette: {
 		background: {
 			default: "#f5f5f5",
 			paper: "#ffffff",
+			black: "#000",
+			white: "fff",
+			disabled: "rgba(224, 224, 224, 1)",
+			border: "rgba(196, 196, 196, 1)",
+			hover: "rgba(247, 248, 252, 1)",
+			selected: "rgba(240, 241, 249, 1)",
 		},
 		text: {
 			secondary: "rgba(0,0,0,0.6)",
 			primary: "rgba(0,0,0,0.87)",
 			disabled: "rgba(0,0,0,0.38)",
+			"25p": "rgba(245, 245, 245, 1)",
+			"50p": "rgba(144, 144, 144, 1)",
+			white: "#fff",
 		},
 		divider: "rgba(0,0,0,0.12)",
 		primary: {
@@ -17,47 +53,53 @@ const theme = createTheme({
 			contrastText: "#ffffff",
 			dark: "#002884",
 			light: "#757de8",
+			"50p": "rgba(159, 168, 218, 1)",
+			"25p": "rgba(207, 212, 237, 1)",
 		},
 		secondary: {
 			main: "#daefef",
 			dark: "#a8bdbd",
 			light: "#e9ffff",
 			contrastText: "#1d1d1d",
+			"50p": "rgba(237, 247, 247, 1)",
+			"25p": "rgba(246, 251, 251, 1)",
 		},
 		error: {
 			main: "#d32f2f",
 			dark: "#c62828",
 			light: "#ef5350",
 			contrastText: "#ffffff",
+			"50p": "rgba(233, 151, 151, 1)",
+			"25p": "rgba(244, 203, 203, 1)",
 		},
 		warning: {
 			main: "#ed6c02",
 			dark: "#e65100",
 			light: "#ff9800",
 			contrastText: "#ffffff",
+			"50p": "rgba(246, 182, 128, 1)",
+			"25p": "rgba(251, 218, 192, 1)",
 		},
 		info: {
 			main: "#0288d1",
 			dark: "#01579b",
 			light: "#03a9f4",
 			contrastText: "#ffffff",
+			"50p": "rgba(128, 195, 232, 1)",
+			"25p": "rgba(192, 225, 244, 1)",
 		},
 		success: {
 			main: "#2e7d32",
 			dark: "#1b5e20",
 			light: "#4caf50",
 			contrastText: "#ffffff",
+			"50p": "rgba(192, 225, 244, 1)",
+			"25p": "rgba(203, 223, 204, 1)",
 		},
 	},
 	typography: {
 		fontSize: 14, // 1 rem is still 16px
-		fontFamily: [
-			"'Source Sans 3'",
-			"'Source Sans Pro'",
-			"'Helvetica Neue'",
-			"Arial",
-			"sans-serif",
-		].join(","),
+		fontFamily: ["'Source Sans 3'", "'Source Sans Pro'", "'Helvetica Neue'", "Arial", "sans-serif"].join(","),
 		body1: {
 			fontWeight: 400,
 			fontSize: 16,
@@ -137,12 +179,13 @@ const theme = createTheme({
 			],
 		},
 	},
-});
+})
 
-declare module '@mui/material/Paper' {
+declare module "@mui/material/Paper" {
 	interface PaperPropsVariantOverrides {
 		padded: true
 	}
 }
 
-export default theme;
+
+export default theme
