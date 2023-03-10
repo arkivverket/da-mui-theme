14e863c5866398c710dc6d8b0420e87af6b7adb4
Sigmund Lahn <siglah@arkivverket.no>
2023-03-10 13:01:20 +0100

    Improve/expand gallery

--- gallery/src/components/Colors.tsx
+++ gallery/src/components/Colors.tsx
@@ -1,5 +1,4 @@
-import { Box, Grid, Paper, Stack, styled, Typography } from "@mui/material"
-import { SystemProps } from "@mui/system"
+import { Box, Grid, Paper, Stack, StackProps, styled, Typography } from "@mui/material"
 
 const ColorDropRectangle = styled("div")({
 	boxSizing: "border-box",
@@ -8,6 +7,14 @@ const ColorDropRectangle = styled("div")({
 	border: "2px solid #F5F5F5",
 	boxShadow: "-4px 0px 4px rgba(0, 0, 0, 0.05), 0px 4px 4px rgba(0, 0, 0, 0.1)",
 	borderRadius: "20px 20px 20px 0px",
+
+	width: "50px",
+	height: "50px",
+
+	"&.big": {
+		width: "100px",
+		height: "100px",
+	},
 })
 
 const ColorDropStack = styled(Stack)(({ theme }) => ({
@@ -15,11 +22,10 @@ const ColorDropStack = styled(Stack)(({ theme }) => ({
 	borderWidth: "1px",
 	borderStyle: "solid",
 	padding: "12px",
-	margin: "12px",
 	alignItems: "center",
 	justifyContent: "center",
 
-	"&:first-child": {
+	"&:first-of-type": {
 		marginLeft: "0px",
 	},
 
@@ -29,13 +35,12 @@ const ColorDropStack = styled(Stack)(({ theme }) => ({
 	},
 }))
 
-const ColorDrop = ({ name, path, size, ...props }: { name: string; path: string; size: number } & SystemProps<{}>) => {
+const ColorDrop = ({ name, path, big, ...props }: { big?: boolean; name: string; path: string } & StackProps) => {
 	const paletteName = `${path}.${name.toLowerCase()}`
-	const boxSize = `${size}px`
 
 	return (
 		<ColorDropStack {...props}>
-			<ColorDropRectangle sx={{ backgroundColor: paletteName, height: boxSize, width: boxSize }} />
+			<ColorDropRectangle sx={{ backgroundColor: paletteName }} className={big ? "big" : ""} />
 			<Typography variant="body1">{name}</Typography>
 		</ColorDropStack>
 	)
@@ -45,13 +50,50 @@ const PaletteDisplay = ({ name, path }: { name: string; path: string }) => {
 	return (
 		<Stack sx={{ maxWidth: { xs: "100%", md: "80%", xl: "60%" } }}>
 			<Typography variant="h4">{name}</Typography>
-			<Box display="grid" gridTemplateAreas={'"a a b c" "a a d e"'}>
-				<ColorDrop path={path} name="Main" size={100} gridArea="a" />
-				<ColorDrop path={path} name="Dark" size={50} gridArea="b" />
-				<ColorDrop path={path} name="50p" size={50} gridArea="c" />
-				<ColorDrop path={path} name="Light" size={50} gridArea="d" />
-				<ColorDrop path={path} name="25p" size={50} gridArea="e" />
+			<Box display="grid" gridTemplateAreas={{ xs: '"a a" "b c" "d e"', md: '"a a b c" "a a d e"' }}>
+				<ColorDrop path={path} name="Main" big gridArea="a" />
+				<ColorDrop path={path} name="Dark" gridArea="b" />
+				<ColorDrop path={path} name="50p" gridArea="c" />
+				<ColorDrop path={path} name="Light" gridArea="d" />
+				<ColorDrop path={path} name="25p" gridArea="e" />
 			</Box>
+		</Stack>
+	)
+}
+
+const TextPaletteDisplay = ({}) => {
+	return (
+		<Stack sx={{ maxWidth: { xs: "100%", md: "80%", xl: "60%" } }}>
+			<Typography variant="h4">Text</Typography>
+			<Box display="grid" gridTemplateAreas={'"a a b c" "a a d e" "a a f g"'}>
+				<ColorDrop path={"text"} name="Primary" big gridArea="a" />
+				<ColorDrop path={"text"} name="Secondary" gridArea="b" />
+				<ColorDrop path={"text"} name="50p" gridArea="c" />
+				<ColorDrop path={"text"} name="Disabled" gridArea="d" />
+				<ColorDrop path={"text"} name="25p" gridArea="e" />
+				<ColorDrop path={"text"} name="White" gridArea="f" />
+			</Box>
+		</Stack>
+	)
+}
+
+const BackgroundPaletteDisplay = ({}) => {
+	const dropSx = {
+		justifyContent: "flex-start",
+	}
+
+	return (
+		<Stack sx={{ maxWidth: { xs: "100%", md: "80%", xl: "60%" } }}>
+			<Typography variant="h4">Backgrounds</Typography>
+			<Stack>
+				<ColorDrop path={"background"} name="Black" direction={"row"} sx={{ ...dropSx }} />
+				<ColorDrop path={"background"} name="White" direction={"row"} sx={{ ...dropSx }} />
+				<ColorDrop path={"background"} name="Disabled" direction={"row"} sx={{ ...dropSx }} />
+				<ColorDrop path={"background"} name="Border" direction={"row"} sx={{ ...dropSx }} />
+				<ColorDrop path={"background"} name="Hover" direction={"row"} sx={{ ...dropSx }} />
+				<ColorDrop path={"background"} name="Selected" direction={"row"} sx={{ ...dropSx }} />
+				<ColorDrop path={"background"} name="Primary" direction={"row"} sx={{ ...dropSx }} />
+			</Stack>
 		</Stack>
 	)
 }
@@ -73,6 +115,20 @@ export const Colors = () => {
 				</Grid>
 				<Grid item xs={6}>
 					<PaletteDisplay name="Error" path="error" />
+				</Grid>
+
+				<Grid item xs={6}>
+					<TextPaletteDisplay />
+				</Grid>
+
+				<Grid item xs={6}>
+					<PaletteDisplay name="Warning" path="warning" />
+				</Grid>
+				<Grid item xs={6}>
+					<PaletteDisplay name="Success" path="success" />
+				</Grid>
+				<Grid item xs={6}>
+					<BackgroundPaletteDisplay />
 				</Grid>
 			</Grid>
 		</Paper>
--- gallery/src/components/Gallery.tsx
+++ gallery/src/components/Gallery.tsx
@@ -1,11 +1,13 @@
 import { Container, Stack, Typography } from "@mui/material"
 import { Colors } from "./Colors"
+import { SpacingExample } from "./SpacingExample"
 
 export const Gallery = () => (
 	<Container maxWidth="xl">
 		<Typography variant="h1">Arkivverket MUI theme</Typography>
 		<Stack>
 			<Colors />
+			<SpacingExample />
 		</Stack>
 	</Container>
 )
