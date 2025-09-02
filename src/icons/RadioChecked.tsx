e8de60efbaac43c31dfa8451b4793f61112481d4
Frans Joakim Titulaer <fransjlt@gmail.com>
2025-09-02 14:41:08 +0200

    3.1.3

--- src/icons/RadioChecked.tsx
+++ src/icons/RadioChecked.tsx
@@ -7,9 +7,13 @@ const RadioCheckedIcon: OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
 } = Object.assign(
 	(props: SvgIconProps) => (
 		<SvgIcon {...props} viewBox="0 0 24 24">
-			<circle cx="12" cy="12" r="10" fill="white" />
-			<circle cx="12" cy="12" r="6" fill="currentColor" />
-			<circle cx="12" cy="12" r="11.5" fill="none" stroke="#1D1D1D" strokeWidth={1} />
+			<path
+				d="M12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1Z"
+				fill="white"
+				stroke="#0D0D0D"
+				strokeWidth={2}
+			/>
+			<circle cx="12" cy="12" r="6" fill="#0D0D0D" />
 		</SvgIcon>
 	),
 	{ muiName: "RadioCheckedIcon" },
--- src/icons/RadioUnchecked.tsx
+++ src/icons/RadioUnchecked.tsx
@@ -7,8 +7,12 @@ const RadioUncheckedIcon: OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
 } = Object.assign(
 	(props: SvgIconProps) => (
 		<SvgIcon {...props} viewBox="0 0 24 24">
-			<circle cx="12" cy="12" r="10" fill="white" />
-			<circle cx="12" cy="12" r="11.5" fill="none" stroke="#1D1D1D" strokeWidth={1} />
+			<path
+				d="M11.5 0.8C17.2 0.8 22.2 5.8 22.2 11.5C22.2 17.2 17.2 22.2 11.5 22.2C5.8 22.2 0.8 17.2 0.8 11.5C0.8 5.8 5.8 0.8 11.5 0.8Z"
+				fill="white"
+				stroke="#1D1D1D"
+				strokeWidth={1}
+			/>
 		</SvgIcon>
 	),
 	{ muiName: "RadioUncheckedIcon" },
