1fcb2c778e2ca513db47233b215ee8b73bbf44c0
Frans Joakim Titulaer <fransjlt@gmail.com>
2025-09-02 16:21:35 +0200

    wip

--- src/icons/RadioChecked.tsx
+++ src/icons/RadioChecked.tsx
@@ -7,13 +7,8 @@ const RadioCheckedIcon: OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
 } = Object.assign(
 	(props: SvgIconProps) => (
 		<SvgIcon {...props} viewBox="0 0 24 24">
-			<path
-				d="M12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1Z"
-				fill="white"
-				stroke="#0D0D0D"
-				strokeWidth={2}
-			/>
-			<circle cx="12" cy="12" r="6" fill="#0D0D0D" />
+			<circle cx="14" cy="12" r="9" fill="white" stroke="#0D0D0D" strokeWidth={2} />
+			<circle cx="14" cy="12" r="5" fill="#0D0D0D" />
 		</SvgIcon>
 	),
 	{ muiName: "RadioCheckedIcon" },
